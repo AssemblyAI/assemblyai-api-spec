3ccc06b92d52a7ed79337e8d79dcd8d77578e19e
Martin Schweiger <martinschweigerod@gmail.com>
2025-02-04 07:55:57 +0800

    implement kapa

---
+++ fern/kapa.js
@@ -0,0 +1,9 @@
+const script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.async = true;
+script.setAttribute("data-website-id", "42353092-36d7-42bd-a213-6fd7af0de0cd");
+script.setAttribute("data-project-name", "AssemblyAI");
+script.setAttribute("data-project-color", "#2C4BD4");
+script.setAttribute("data-project-logo", "../../assets/img/logo-blue400x400.jpeg");
+script.setAttribute("data-modal-override-open-id-search", "fern-search-button");
+document.head.appendChild(script);
