d0d673901ee1e271c32e362016b41c2f529f5c4f
Martin Schweiger <34636718+m-ods@users.noreply.github.com>
2025-02-25 10:19:37 +0800

    feat: add kapa event tracking for search interactions (#178)
    * add kapa event tracking for search interactions
    
    * add kapa search type
    
    ---------
    
    Co-authored-by: Ryan Seams <ryanseams@Mac.attlocal.net>

--- fern/kapa.js
+++ fern/kapa.js
@@ -12,17 +12,95 @@
   }
 })();
 
-window.Kapa("onSearchResultsCompleted", () => {
+// track event for kapa modal opening
+window.Kapa("onModalOpen", function ({ mode }) {
+  rudderanalytics.track("docs_click_search-open",
+    {
+      "search_mode": mode,
+    });
+  if(rudderanalytics.getUserId()!=='') {
+    window.kapaSettings = {
+      user: {
+        uniqueClientId: rudderanalytics.getUserId(),
+        email: rudderanalytics.getUserTraits().email
+      }
+    }
+  }
+});
+
+// track event for kapa modal closing
+window.Kapa("onModalClose", function ({ mode }) {
+  rudderanalytics.track("docs_click_search-close",
+    {
+      "search_mode": mode,
+    });
+});
+
+// track event for kapa modal search completing
+window.Kapa("onSearchResultsCompleted", ({ query, searchResults }) => {
   const searchInput = document
     .querySelector("#kapa-widget-container")
     .shadowRoot.querySelector("input[type=text]");
-  const query = searchInput.value;
+  rudderanalytics.track("docs_completed_search",
+    {
+      "search_query": query,
+      "search_results": searchResults,
+      "clicked_url": searchResults.url,
+      "clicked_title": searchResults.title,
+    });
   searchInput.addEventListener("keyup", function handleQuery(event) {
     this.removeEventListener("keyup", handleQuery);
     if (event.key === "Enter") {
       window.Kapa("open", { mode: "ai", query: query, submit: true });
     }
   });
+});
+
+// track event for kapa modal search result clicked from list
+window.Kapa("onSearchResultClick", ({ query, searchResult, rank }) => {
+  rudderanalytics.track("docs_click_search-result",
+    {
+      "search_query": query,
+      "search_results": searchResult,
+      "search_rank": rank,
+      "clicked_url": searchResult.url,
+      "clicked_title": searchResult.title,
+    });
+});
+
+// track event for kapa modal search clicking "ask ai" in search results
+window.Kapa("onAskAIQuerySubmit", ({ threadId, questionAnswerId, question }) => {
+  rudderanalytics.track("docs_click_search-ask-ai",
+    {
+      "search_query": question,
+    });
+});
+
+// track event for kapa modal search result clicking "ask ai" completing
+window.Kapa("onAskAIAnswerCompleted", ({ threadId, questionAnswerId, question, answer, conversation }) => {
+  rudderanalytics.track("docs_completed_search-ask-ai",
+    {
+      "search_query": question,
+    });
+});
+
+// track event for kapa modal search result clicked from "ask ai" answer text
+window.Kapa("onAskAILinkClick", ({ href, threadId, questionAnswerId, question, answer }) => {
+  rudderanalytics.track("docs_click_search-ask-ai-result",
+    {
+      "search_query": question,
+      "clicked_url": href,
+    });
+});
+
+// track event for kapa modal search result clicked from "ask ai" answer sources
+window.Kapa("onAskAISourceClick", ({ source, threadId, questionAnswerId, question, answer }) => {
+  rudderanalytics.track("docs_click_search-ask-ai-source",
+    {
+      "search_query": question,
+      "clicked_url": source.url,
+      "clicked_title": source.title,
+    });
 });
 
 function insertKapa() {
@@ -42,6 +120,7 @@ function insertKapa() {
   script.setAttribute("data-project-logo", "https://www.assemblyai.com/static/images/logo-blue400x400.jpeg");
   script.setAttribute("data-modal-override-open-id-search", "fern-search-button");
   script.setAttribute("data-search-include-source-names", '["Pylon FAQ", "Documentation"]');
+  script.setAttribute("data-user-analytics-fingerprint-enabled", true);
   document.head.appendChild(script);
   
   document.addEventListener('keydown', (e) => {
