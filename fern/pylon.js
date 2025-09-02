fe431a6d5bc8e52617e1790a03a2bb0b377ac347
Martin Schweiger <34636718+m-ods@users.noreply.github.com>
2025-09-02 20:37:54 +0800

    feat: add pylon (#393)
    * feat: add pylon
    
    * code fixes

---
+++ fern/pylon.js
@@ -0,0 +1,49 @@
+(function () {
+  var e = window;
+  var t = document;
+  var n = function () {
+    n.e(arguments);
+  };
+  n.q = [];
+  n.e = function (e) {
+    n.q.push(e);
+  };
+  e.Pylon = n;
+  var r = function () {
+    var e = t.createElement("script");
+    e.setAttribute("type", "text/javascript");
+    e.setAttribute("async", "true");
+    e.setAttribute(
+      "src",
+      "https://widget.usepylon.com/widget/f28d5a70-a10d-4a6c-bd5e-cff70ac09f60"
+    );
+    var n = t.getElementsByTagName("script")[0];
+    n.parentNode.insertBefore(e, n);
+  };
+  if (t.readyState === "complete") {
+    r();
+  } else if (e.addEventListener) {
+    e.addEventListener("load", r, false);
+  }
+})();
+
+// fetch pylon credentials (email, and email_hash) based on user's auth cookie and show chat bubble
+(function () {
+  fetch("https://www.assemblyai.com/dashboard/api/pylon-credentials", {
+    // will no longer be needed when we're on the same domain
+    credentials: "include",
+  })
+    .then((res) => {
+      if (res && res.email && res.emailHash) {
+        window.pylon = {
+          chat_settings: {
+            app_id: "f28d5a70-a10d-4a6c-bd5e-cff70ac09f60",
+            name: "User",
+            email: res.email,
+            email_hash: res.emailHash,
+          },
+        };
+      }
+    })
+    .catch((error) => {});
+})();
